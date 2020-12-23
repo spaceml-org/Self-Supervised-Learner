0a31fde946cf997c63f956fb9efd0638e65369ea
RudyVenguswamy <47090043+RudyVenguswamy@users.noreply.github.com>
2020-12-23 10:47:48 -0800

    Update ssl_dali_distrib.py

--- ssl_dali_distrib.py
+++ ssl_dali_distrib.py
@@ -61,11 +61,11 @@ class sslSIMCLR(SimCLR):
  
       
   def setup(self, stage = None):
-      print(self.device, "_+__++_+__+__++_+__+__++_+__+__++_+__+__++_+__+__++_+_")
+      
       self.num_samples = sum([len(files) for r, d, files in os.walk(f'{self.DATA_PATH}/train')])
       #model stuff    
       super().__init__(gpus = self.gpus, num_samples = self.num_samples, batch_size = self.batch_size, dataset = 'None', max_epochs = self.epochs)
-      print(self.encoder_name)
+      print(self.device, "_+__++_+__+__++_+__+__++_+__+__++_+__+__++_+__+__++_+_")
       self.encoder, self.embedding_size = load_encoder(self.encoder_name, self.kwargs)
       
       print(self.DATA_PATH)
