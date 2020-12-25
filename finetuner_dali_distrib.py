cc37293d45d1576ee6a2d6e8c13d7c7b2a88ea79
RudyVenguswamy <47090043+RudyVenguswamy@users.noreply.github.com>
2020-12-25 10:21:05 -0800

    Update finetuner_dali_distrib.py

--- finetuner_dali_distrib.py
+++ finetuner_dali_distrib.py
@@ -40,7 +40,7 @@ from torch.optim import SGD
 class finetuneSIMCLR(pl.LightningModule):
 
   def __init__(self, encoder, DATA_PATH, withhold, batch_size, val_split, hidden_dims, train_transform, val_transform, num_workers, **kwargs):
-      super().__init__()
+      
 
       self.DATA_PATH = DATA_PATH
       self.val_split = val_split
@@ -57,7 +57,9 @@ class finetuneSIMCLR(pl.LightningModule):
           splitfolders.ratio(self.DATA_PATH, output=f"split_data", ratio=(1-self.val_split-self.withhold, self.val_split, self.withhold), seed = 10)
           self.DATA_PATH = 'split_data'
           print(f'automatically splitting data into train and validation data {self.val_split} and withhold {self.withhold}')
-
+          
+  def setup(self, stage = None):
+      super().__init__()
       self.num_classes = len(os.listdir(f'{self.DATA_PATH}/train'))
 
       #model stuff    
@@ -72,6 +74,22 @@ class finetuneSIMCLR(pl.LightningModule):
             p=0.1,
             n_hidden=self.hidden_dims
        )
+      
+      train_pipeline = self.train_transform(DATA_PATH = f"{self.DATA_PATH}/train", input_height = 256, batch_size = self.batch_size, num_threads = self.num_workers, device_id = self.global_rank)
+      print(f"{self.DATA_PATH}/train")
+      val_pipeline = self.val_transform(DATA_PATH = f"{self.DATA_PATH}/val", input_height = 256, batch_size = self.batch_size, num_threads = self.num_workers, device_id = self.global_rank)
+
+      class LightningWrapper(DALIClassificationIterator):
+          def __init__(self, *kargs, **kvargs):
+              super().__init__(*kargs, **kvargs)
+
+          def __next__(self):
+              out = super().__next__()
+              out = out[0]
+              return [out[k] if k != "label" else torch.squeeze(out[k]) for k in self.output_map]
+
+      self.train_loader = LightningWrapper(train_pipeline, fill_last_batch=False, auto_reset=True, reader_name = "Reader")
+      self.val_loader = LightningWrapper(val_pipeline, fill_last_batch=False, auto_reset=True, reader_name = "Reader")
 
 
 #   def forward(self, x):
@@ -119,24 +137,6 @@ class finetuneSIMCLR(pl.LightningModule):
       
       return [opt]
   
-  
-  def prepare_data(self):
-
-      train_pipeline = self.train_transform(DATA_PATH = f"{self.DATA_PATH}/train", input_height = 256, batch_size = self.batch_size, num_threads = self.num_workers, device_id = 0)
-      print(f"{self.DATA_PATH}/train")
-      val_pipeline = self.val_transform(DATA_PATH = f"{self.DATA_PATH}/val", input_height = 256, batch_size = self.batch_size, num_threads = self.num_workers, device_id = 0)
-
-      class LightningWrapper(DALIClassificationIterator):
-          def __init__(self, *kargs, **kvargs):
-              super().__init__(*kargs, **kvargs)
-
-          def __next__(self):
-              out = super().__next__()
-              out = out[0]
-              return [out[k] if k != "label" else torch.squeeze(out[k]) for k in self.output_map]
-
-      self.train_loader = LightningWrapper(train_pipeline, fill_last_batch=False, auto_reset=True, reader_name = "Reader")
-      self.val_loader = LightningWrapper(val_pipeline, fill_last_batch=False, auto_reset=True, reader_name = "Reader")
 
 
   def train_dataloader(self):
@@ -162,6 +162,8 @@ def cli_main():
     parser.add_argument("--eval", default=True, type=bool, help="Eval Mode will train and evaluate the finetuned model's performance")
     parser.add_argument("--pretrain_encoder", default=False, type=bool, help="initialize resnet encoder with pretrained imagenet weights. Ignored if MODEL_PATH is specified.")
     parser.add_argument("--version", default="0", type=str, help="version to name checkpoint for saving")
+    parser.add_argument("--log_name", type=str, help="name of project to log on wandb")
+    parser.add_argument("--online_eval", default=False, type=bool, help="Do finetuning on model if labels are provided as a sanity check")
     
     args = parser.parse_args()
     DATA_PATH = args.DATA_PATH
@@ -180,16 +182,26 @@ def cli_main():
     version = args.version
     pretrain = args.pretrain_encoder
     encoder = args.encoder
+    log_name = args.log_name
+    online_eval = args.online_eval
     
-    
+    wandb_logger = WandbLogger(name=log_name,project='SpaceForce')
     model = finetuneSIMCLR(encoder = encoder, MODEL_PATH = MODEL_PATH, withhold = withhold, pretrained = pretrain, DATA_PATH  = DATA_PATH, batch_size = batch_size, val_split = val_split, hidden_dims = hidden_dims, train_transform = SimCLRFinetuneTrainDataTransform, val_transform = SimCLRFinetuneTrainDataTransform, num_workers = num_workers)
+    
+    cbs = []
+    backend = 'dp'
+    
     if patience > 0:
-        cb = EarlyStopping('val_acc_epoch', patience = patience, mode='max')
-        trainer = Trainer(gpus=gpus, max_epochs = epochs, callbacks=[cb], progress_bar_refresh_rate=5)
-    else:
-        trainer = Trainer(gpus=gpus, max_epochs = epochs, progress_bar_refresh_rate=5)
-
-    trainer.fit(model)
+        cb = EarlyStopping('val_loss', patience = patience)
+        cbs.append(cb)
+    
+    if online_eval:
+        cbs.append(online_evaluator)
+        backend = 'ddp'
+        
+    trainer = Trainer(gpus=gpus, max_epochs = epochs, progress_bar_refresh_rate=5, callbacks = cbs, distributed_backend=f'{backend}' if args.gpus > 1 else None, logger = wandb_logger, enable_pl_optimizer=True)
+    
+    print('USING BACKEND______________________________ ', backend)
     Path(f"./models/Finetune/SIMCLR_Finetune_{version}").mkdir(parents=True, exist_ok=True)
     torch.save(model.state_dict(), f"./models/Finetune/SIMCLR_Finetune_{version}/SIMCLR_FINETUNE_{version}.pt")
     
