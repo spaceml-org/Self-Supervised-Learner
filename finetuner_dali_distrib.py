9a256eb5d26385a98c2d5aff2d541a747ab114d8
RudyVenguswamy <47090043+RudyVenguswamy@users.noreply.github.com>
2021-01-02 13:59:55 -0800

    Update finetuner_dali_distrib.py

--- finetuner_dali_distrib.py
+++ finetuner_dali_distrib.py
@@ -12,7 +12,7 @@ import os
 from os import path
 import splitfolders
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
-from pl_bolts.models.self_supervised.evaluator import SSLEvaluator
+#from pl_bolts.models.self_supervised.evaluator import SSLEvaluator
 
 from pytorch_lightning.loggers import WandbLogger
 
@@ -32,6 +32,44 @@ from torch import nn
 from torch.nn import functional as F
 from torch import nn
 from torch.optim import SGD
+
+
+class SSLEvaluator(nn.Module):
+    def __init__(self, n_input, n_classes, n_hidden=512, p=0.1):
+        super().__init__()
+        self.n_input = n_input
+        self.n_classes = n_classes
+        self.n_hidden = n_hidden
+        if n_hidden is None:
+            # use linear classifier
+            self.block_forward = nn.Sequential(
+                Flatten(),
+                #nn.Dropout(p=p),
+                nn.Linear(n_input, n_classes, bias=True)
+            )
+        else:
+            # use simple MLP classifier
+            self.block_forward = nn.Sequential(
+                Flatten(),
+                nn.Dropout(p=p),
+                nn.Linear(n_input, n_hidden, bias=False),
+                #nn.BatchNorm1d(n_hidden),
+                nn.ReLU(inplace=True),
+                #nn.Dropout(p=p),
+                nn.Linear(n_hidden, n_classes, bias=True)
+            )
+
+    def forward(self, x):
+        logits = self.block_forward(x)
+        return logits
+
+
+class Flatten(nn.Module):
+    def __init__(self):
+        super(Flatten, self).__init__()
+
+    def forward(self, input_tensor):
+        return input_tensor.view(input_tensor.size(0), -1)
 
 class finetuner(pl.LightningModule):
 
