31c4e49f0a53703d362e77efb521eb0d96e68830
RudyVenguswamy <47090043+RudyVenguswamy@users.noreply.github.com>
2021-01-03 12:41:23 -0800

    Update transforms_dali.py

--- transforms_dali.py
+++ transforms_dali.py
@@ -12,19 +12,20 @@ class SimCLRFinetuneTrainDataTransform(Pipeline):
 
         self.input_height = input_height
         self.input = ops.FileReader(file_root = DATA_PATH, random_shuffle = True, seed = 12)
-
+        self.to_int64 = ops.Cast(dtype=types.INT64, device="gpu")
+        
         self.coin = ops.CoinFlip(probability=0.5)
         self.uniform = ops.Uniform(range = [0.7,1.3]) #-1 to 1
         self.blur_amt = ops.Uniform(values = [float(i) for i in range(1, int(0.1*self.input_height), 2)])
         #read image (I think that has to be cpu, do a mixed operation to decode into gpu)
         self.decode = ops.ImageDecoder(device = 'mixed', output_type = types.RGB)
-        self.crop = ops.RandomResizedCrop(size = self.input_height, minibatch_size = batch_size, device = "gpu", dtype = types.FLOAT)
+        self.crop = ops.RandomResizedCrop(size = self.input_height, minibatch_size = batch_size, device = "gpu")
         self.flip = ops.Flip(vertical = self.coin(), horizontal = self.coin(), device = "gpu")
         self.colorjit_gray = ops.ColorTwist(brightness = self.uniform(), contrast = self.uniform(), hue = self.uniform(), saturation = self.uniform(), device = "gpu", dtype = types.FLOAT)
-        self.blur = ops.GaussianBlur(window_size = 3, device = "gpu")
+        self.blur = ops.GaussianBlur(window_size = self.to_int64(self.blur_amt()), device = "gpu", dtype = types.FLOAT)
         self.swapaxes = ops.Transpose(perm = [2,0,1], device = "gpu")
 
-        self.to_int64 = ops.Cast(dtype=types.INT64, device="gpu")
+        
 
     def define_graph(self):
         jpegs, labels = self.input(name = 'Reader')
