a212bde33cf104159e88de400623e439eefbd4da
RudyVenguswamy <47090043+RudyVenguswamy@users.noreply.github.com>
2021-01-03 16:57:38 -0800

    Update encoders_dali.py

--- encoders_dali.py
+++ encoders_dali.py
@@ -7,47 +7,44 @@ from pl_bolts.models.self_supervised.resnets import resnet18, resnet50
 
 class miniCNN(nn.Module):
 
-  def __init__(self):
+  def __init__(self, output_dim):
     super().__init__()
-
+    self.output_dim = output_dim
     # merced images are (3, 256, 256) (channels, width, height)
-    self.conv1 = nn.Conv2d(in_channels = 3, out_channels = 16, kernel_size = 3, stride = 1, padding = 0)
-    self.conv2 = nn.Conv2d(in_channels = 16, out_channels = 32, kernel_size = 5, stride = 1, padding = 0)
-    self.conv3 = nn.Conv2d(in_channels = 32, out_channels = 48, kernel_size = 3, stride = 2, padding = 0)
-    self.conv4 = nn.Conv2d(in_channels = 48, out_channels = 64, kernel_size = 3, stride = 2, padding = 0)
+    self.conv1 = nn.Conv2d(in_channels = 3, out_channels = 16, kernel_size = 3, stride = 1, padding = 1)
+    self.conv2 = nn.Conv2d(in_channels = 16, out_channels = 32, kernel_size = 3, stride = 2, padding = 1)
+    self.conv3 = nn.Conv2d(in_channels = 32, out_channels = 48, kernel_size = 3, stride = 2, padding = 1)
+    self.adaptive_pool = nn.AdaptiveAvgPool2d(output_size = (16, 16))
+    self.conv4 = nn.Conv2d(in_channels = 48, out_channels = 64, kernel_size = 5, stride = 2, padding = 2)
     self.pool = nn.MaxPool2d(2, 2)
-    self.fc1 = nn.Linear(64*3*3, 128)
+    self.flatten = nn.Flatten()
+    self.fc1 = nn.Linear(64*4*4, self.output_dim)
 
     self.train_acc = Accuracy()
     self.val_acc = Accuracy(compute_on_step=False)
     self.test_acc = Accuracy(compute_on_step=False)
 
   def forward(self, x):
-      # print(x.shape)
       x = F.relu(self.conv1(x))
-      # print(x.shape)
       x = self.pool(x)
-      # print(x.shape)
       x = F.relu(self.conv2(x))
-      # print(x.shape)
       x = self.pool(x)
-      # print(x.shape)
       x = F.relu(self.conv3(x))
-      x = self.pool(x)
-      # print(x.shape)
+      x = F.relu(self.adaptive_pool(x))
       x = F.relu(self.conv4(x))
       x = self.pool(x)
-      # print(x.shape)
-      x = x.view(-1 , 64*3*3)
-      # print(x.shape)
-
+      x = self.flatten(x)
+      x = F.relu(self.fc1(x))
       return [x]
 
 
 def load_encoder(encoder_name):
     
-    if encoder_name == 'minicnn':
-        model, embedding_size = miniCNN(), 576
+    if 'minicnn' in encoder_name:
+        #special case to make minicnn output variable output embedding size depending on user arg
+        output_size =  int(''.join(x for x in encoder_name if x.isdigit()))
+        model, embedding_size = miniCNN(output_size), output_size
+        
     elif encoder_name == 'resnet18':
         model, embedding_size = resnet18(pretrained=False, first_conv=True, maxpool1=True, return_all_feature_maps=False), 512
     elif encoder_name == 'imagenet_resnet18':
