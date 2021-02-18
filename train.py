54261dd5557b8aab7f8ae15e7a890cfb5a75a2d1
RudyVenguswamy <47090043+RudyVenguswamy@users.noreply.github.com>
2021-02-18 14:47:41 -0800

    Update train.py

--- train.py
+++ train.py
@@ -17,7 +17,7 @@ from pytorch_lightning.loggers import WandbLogger
 from argparse import ArgumentParser
 
 #Internal Package Imports
-from models import *
+from models import SIMCLR
 
 def load_model(parser):
     '''
