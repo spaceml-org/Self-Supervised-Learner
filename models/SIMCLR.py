192eb79a3ed8cb5ecb4662758a71a41c69688d81
RudyVenguswamy <47090043+RudyVenguswamy@users.noreply.github.com>
2021-02-18 15:08:46 -0800

    Update SIMCLR.py

--- models/SIMCLR.py
+++ models/SIMCLR.py
@@ -19,13 +19,13 @@ from pl_bolts.models.self_supervised import SimCLR
 from pl_bolts.models.self_supervised.simclr.simclr_module import Projection
 
 #Internal Imports
-from dali_utils.dali_transforms import SimCLRTrainDataTransform, SimCLRValDataTransform
+from dali_utils.dali_transforms import SimCLRDataTransform
 from dali_utils.setup import setup_dali
 
 
 class SIMCLR(SimCLR):
 
-    def __init__(self, encoder, DATA_PATH, VAL_PATH, hidden_dims, image_size, train_transform = SimCLRTrainDataTransform, val_transform = SimCLRValDataTransform, **simclr_hparams):
+    def __init__(self, encoder, DATA_PATH, VAL_PATH, hidden_dims, image_size, train_transform = SimCLRDataTransform, **simclr_hparams):
 
         self.DATA_PATH = DATA_PATH
         self.VAL_PATH = VAL_PATH
