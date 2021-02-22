75ee11b7c8317bb80f41323f393afe19e8246510
RudyVenguswamy <47090043+RudyVenguswamy@users.noreply.github.com>
2021-02-22 14:14:01 -0800

    Create SIMSIAM.py

---
+++ models/SIMSIAM.py
@@ -0,0 +1,92 @@
+import os
+from termcolor import colored
+import numpy as np
+import math
+from argparse import ArgumentParser
+from termcolor import colored
+
+import torch
+from torch.nn import functional as F
+from torch import nn
+from torchvision.datasets import ImageFolder
+
+import pytorch_lightning as pl
+from pl_bolts.models.self_supervised import SimSiam
+#from pl_bolts.models.self_supervised.simclr.simclr_module import Projection
+
+#Internal Imports
+from dali_utils.dali_transforms import SimCLRTransform #same transform as SimCLR
+from dali_utils.lightning_compat import SimCLRWrapper
+
+class SIMSIAM(SimSiam):
+
+    def __init__(self, encoder, DATA_PATH, VAL_PATH, hidden_dims, image_size, seed, cpus, transform = SimCLRTransform, **simsiam_hparams):
+        
+        data_temp = ImageFolder(DATA_PATH)
+        
+        #derived values (not passed in) need to be added to model hparams
+        simclr_hparams['num_samples'] = len(data_temp)
+        simclr_hparams['dataset'] = None
+        
+        self.DATA_PATH = DATA_PATH
+        self.VAL_PATH = VAL_PATH
+        self.hidden_dims = hidden_dims
+        self.transform = transform
+        self.image_size = image_size
+        self.num_classes = len(data_temp.classes)
+        self.cpus = cpus
+        self.seed = seed
+        self.encoder = encoder
+        
+        super().__init__(**simsiam_hparams)
+
+        self.save_hyperparameters()
+        print('saved hparams here')
+        print(self.hparams)
+  
+    #override pytorch SimSiam with our own encoder so we will overwrite the function plbolts calls to init the encoder
+    def init_model(self):
+        self.online_network = SiameseArm(
+            self.encoder, input_dim=self.hidden_mlp, hidden_size=self.hidden_mlp, output_dim=self.feat_dim
+        )
+
+
+    def setup(self, stage = 'inference'):
+        if stage == 'fit':
+            train = self.transform(self.DATA_PATH, batch_size = self.batch_size, input_height = self.image_size, copies = 3, stage = 'train', num_threads = self.cpus, device_id = self.local_rank, seed = self.seed)
+            val = self.transform(self.VAL_PATH, batch_size = self.batch_size, input_height = self.image_size, copies = 3, stage = 'validation', num_threads = self.cpus, device_id = self.local_rank, seed = self.seed)
+            self.train_loader = SimCLRWrapper(transform = train)
+            self.val_loader = SimCLRWrapper(transform = val)
+            
+        elif stage == 'inference':
+            self.test_dataloader = SimCLRWrapper(transform = self.transform(self.DATA_PATH, batch_size = self.batch_size, input_height = self.image_size, copies = 1, stage = 'inference', num_threads = 2*self.cpus, device_id = self.local_rank, seed = self.seed))
+            self.inference_dataloader = self.test_dataloader
+     
+    def train_dataloader(self):
+        return self.train_loader
+  
+    def val_dataloader(self):
+        return self.val_loader
+    
+    #give user permission to add extra arguments for SIMSIAM model particularly. This cannot share the name of any parameters from train.py
+    def add_model_specific_args(parent_parser):
+        parser = ArgumentParser(parents=[parent_parser], add_help=False)
+
+        # things we need to pass into pytorch lightning simsiam model
+
+        parser.add_argument("--hidden_mlp", default=2048, type=int, help="hidden layer dimension in projection head")
+        parser.add_argument("--feat_dim", default=128, type=int, help="feature dimension")
+
+        # training params
+        parser.add_argument("--num_workers", default=8, type=int, help="num of workers per GPU")
+        parser.add_argument("--optimizer", default="adam", type=str, help="choose between adam/sgd")
+        parser.add_argument("--lars_wrapper", action="store_true", help="apple lars wrapper over optimizer used")
+        parser.add_argument("--exclude_bn_bias", action="store_true", help="exclude bn/bias from weight decay")
+        parser.add_argument("--warmup_epochs", default=10, type=int, help="number of warmup epochs")
+
+        parser.add_argument("--temperature", default=0.1, type=float, help="temperature parameter in training loss")
+        parser.add_argument("--weight_decay", default=1e-6, type=float, help="weight decay")
+        parser.add_argument("--start_lr", default=0, type=float, help="initial warmup learning rate")
+        parser.add_argument("--final_lr", type=float, default=1e-6, help="final learning rate")
+        
+        return parser
