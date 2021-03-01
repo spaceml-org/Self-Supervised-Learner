5f0cad9483f908b1ccc5bf3b3bb4d9ce35a02cad
RudyVenguswamy <47090043+RudyVenguswamy@users.noreply.github.com>
2021-03-01 11:42:07 -0800

    Update CLASSIFIER.py

--- models/CLASSIFIER.py
+++ models/CLASSIFIER.py
@@ -14,6 +14,7 @@ from torch.optim import SGD
 import pytorch_lightning as pl
 from pl_bolts.models.self_supervised.ssl_finetuner import SSLFineTuner
 from pl_bolts.models.self_supervised.evaluator import SSLEvaluator
+from pytorch_lightning.metrics import Accuracy
 
 #Internal Imports
 from dali_utils.dali_transforms import SimCLRTransform #same transform as SimCLR, but only 1 copy
