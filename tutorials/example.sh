eb735a76a6c6e8c54301a81a4596b064b50a4a6f
RudyVenguswamy <47090043+RudyVenguswamy@users.noreply.github.com>
2021-03-03 12:09:04 -0800

    Update example.sh

--- tutorials/example.sh
+++ tutorials/example.sh
@@ -5,22 +5,22 @@ wait
 
 echo "downloaded merced data into current directory."
 echo "Training SIMCLR Model"
-python Self-Supervised-Learner/ssl_dali_distrib.py --DATA_PATH UCMerced_LandUse/Images --encoder minicnn32 --batch_size 64 --num_workers 16 --epochs 10 --gpus 1 --log_name example_merced_SSL
+python SSL/train.py --technique SIMCLR --DATA_PATH /content/UCMerced_LandUse/Images --model imagenet_resnet18  --epochs 10 --batch_size 64 --log_name ssl
 
 echo "Stopped SSL training at epoch 10. Will resume now from checkpoint."
-python Self-Supervised-Learner/ssl_dali_distrib.py --DATA_PATH UCMerced_LandUse/Images --encoder models/SSL/SIMCLR_SSL_example_merced_SSL.ckpt --batch_size 64 --num_workers 16 --epochs 5 --gpus 1 --log_name example_merced_SSL_resumed
+python SSL/train.py --technique SIMCLR --DATA_PATH /content/UCMerced_LandUse/Images --model ./models/SIMCLR_ssl.ckpt  --epochs 20 --batch_size 64 --log_name ssl2
 wait
 
-echo "Typically we want to do SIMCLR SSL for > 400 epochs. For this example, let's just move to finetuning now earlier."
+echo "Typically we want to do SIMCLR SSL for > 100 epochs. For this example, let's just move to finetuning now earlier."
 echo "We'll pass in our SSL Model to the finetuner"
-python Self-Supervised-Learner/finetuner_dali_distrib.py --DATA_PATH UCMerced_LandUse/Images --encoder models/SSL/SIMCLR_SSL_example_merced_SSL_resumed.ckpt --batch_size 64 --num_workers 16 --epochs 10 --gpus 1 --log_name example_merced_FT 
+python SSL/train.py --technique CLASSIFIER --DATA_PATH /content/UCMerced_LandUse/Images --model ./models/SIMCLR_ssl2.ckpt  --epochs 10  --log_name ft
 
-echo "Stopping finetuning at epoch 10. Will resume now from checkpoint."
-python Self-Supervised-Learner/finetuner_dali_distrib.py --DATA_PATH UCMerced_LandUse/Images --encoder models/FineTune/FineTune_example_merced_FT.ckpt --batch_size 64 --num_workers 16 --epochs 5 --gpus 1 --log_name example_merced_FT_resumed
+echo "Stopping finetuning classifier at epoch 10. Will resume now from checkpoint."
+python SSL/train.py --technique CLASSIFIER --DATA_PATH /content/UCMerced_LandUse/Images --model ./models/CLASSIFIER_ft.ckpt  --epochs 20  --log_name ft2
 
 echo "Great - to access your classifier, just: "
-echo "from finetuner_dali_distrib import finetuner" 
-echo "model = finetuner.load_from_checkpoint\('models/FineTune/FineTune_example_merced_FT_resumed.ckpt'\)"
+echo "from models import CLASSIFIER" 
+echo "model = CLASSIFIER.load_from_checkpoint\('CLASSIFIER_ft2.ckpt'\)"
 echo "You can use this model like any old pytorch model."
 
 
