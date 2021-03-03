9ac513aa26024e62bc9434647d438ca7e5f06f42
RudyVenguswamy <47090043+RudyVenguswamy@users.noreply.github.com>
2021-03-03 12:11:08 -0800

    Update example.sh

--- tutorials/example.sh
+++ tutorials/example.sh
@@ -5,18 +5,18 @@ wait
 
 echo "downloaded merced data into current directory."
 echo "Training SIMCLR Model"
-python SSL/train.py --technique SIMCLR --DATA_PATH /content/UCMerced_LandUse/Images --model imagenet_resnet18  --epochs 10 --batch_size 64 --log_name ssl
+python Self-Supervised-Learner/train.py --technique SIMCLR --DATA_PATH /content/UCMerced_LandUse/Images --model imagenet_resnet18  --epochs 10 --batch_size 64 --log_name ssl
 
 echo "Stopped SSL training at epoch 10. Will resume now from checkpoint."
-python SSL/train.py --technique SIMCLR --DATA_PATH /content/UCMerced_LandUse/Images --model ./models/SIMCLR_ssl.ckpt  --epochs 20 --batch_size 64 --log_name ssl2
+python Self-Supervised-Learner/train.py --technique SIMCLR --DATA_PATH /content/UCMerced_LandUse/Images --model ./models/SIMCLR_ssl.ckpt  --epochs 20 --batch_size 64 --log_name ssl2
 wait
 
 echo "Typically we want to do SIMCLR SSL for > 100 epochs. For this example, let's just move to finetuning now earlier."
 echo "We'll pass in our SSL Model to the finetuner"
-python SSL/train.py --technique CLASSIFIER --DATA_PATH /content/UCMerced_LandUse/Images --model ./models/SIMCLR_ssl2.ckpt  --epochs 10  --log_name ft
+python Self-Supervised-Learner/train.py --technique CLASSIFIER --DATA_PATH /content/UCMerced_LandUse/Images --model ./models/SIMCLR_ssl2.ckpt  --epochs 10  --log_name ft
 
 echo "Stopping finetuning classifier at epoch 10. Will resume now from checkpoint."
-python SSL/train.py --technique CLASSIFIER --DATA_PATH /content/UCMerced_LandUse/Images --model ./models/CLASSIFIER_ft.ckpt  --epochs 20  --log_name ft2
+python Self-Supervised-Learner/train.py --technique CLASSIFIER --DATA_PATH /content/UCMerced_LandUse/Images --model ./models/CLASSIFIER_ft.ckpt  --epochs 20  --log_name ft2
 
 echo "Great - to access your classifier, just: "
 echo "from models import CLASSIFIER" 
