148f1701616e3e796ce983cb11617011e3dd9c6f
RudyVenguswamy <47090043+RudyVenguswamy@users.noreply.github.com>
2021-03-04 14:21:51 -0800

    Update CLASSIFIER.py

--- models/CLASSIFIER.py
+++ models/CLASSIFIER.py
@@ -71,6 +71,7 @@ class CLASSIFIER(pl.LightningModule): #SSLFineTuner
         return [optimizer], [scheduler]
     
     def forward(self, x):
+        print('GOT HERE FIRST')
         feats = self.encoder(x)[-1]
         print('GOT HERE')
         feats = feats.view(feats.size(0), -1)
