0cac7bb27dd54a5c323c730b062f1bd452ef710e
Ajay Krishnan <42097993+ajaykrishnan23@users.noreply.github.com>
2021-04-02 10:23:38 +0530

    Updated new MiniCNN

--- models/encoders.py
+++ models/encoders.py
@@ -9,24 +9,25 @@ class miniCNN(nn.Module):
     super().__init__()
     self.output_dim = output_dim
 
-    self.conv1 = nn.Conv2d(in_channels = 3, out_channels = 16, kernel_size = 3, stride = 1, padding = 1)
-    self.conv2 = nn.Conv2d(in_channels = 16, out_channels = 32, kernel_size = 3, stride = 2, padding = 1)
-    self.conv3 = nn.Conv2d(in_channels = 32, out_channels = 48, kernel_size = 3, stride = 2, padding = 1)
+    self.conv1 = nn.Conv2d(in_channels = 3, out_channels = 32, kernel_size = 3, stride = 2, padding = 1)
+    self.conv2 = nn.Conv2d(in_channels = 32, out_channels = 56, kernel_size = 3, stride = 2, padding = 1)
+    self.conv3 = nn.Conv2d(in_channels = 56, out_channels = 98, kernel_size = 3, stride = 2, padding = 1)
     self.adaptive_pool = nn.AdaptiveAvgPool2d(output_size = (16, 16))
-    self.conv4 = nn.Conv2d(in_channels = 48, out_channels = 64, kernel_size = 5, stride = 2, padding = 2)
+    self.conv4 = nn.Conv2d(in_channels = 98, out_channels = 64, kernel_size = 5, stride = 2, padding = 2)
     self.pool = nn.MaxPool2d(2, 2)
     self.flatten = nn.Flatten()
-    self.fc1 = nn.Linear(64*4*4, self.output_dim)
+    self.fc1 = nn.Linear(64*8*8, self.output_dim)
 
   def forward(self, x):
       x = F.relu(self.conv1(x))
-      x = self.pool(x)
+      # x = self.pool(x)
       x = F.relu(self.conv2(x))
-      x = self.pool(x)
+      # x = self.pool(x)
       x = F.relu(self.conv3(x))
       x = F.relu(self.adaptive_pool(x))
       x = F.relu(self.conv4(x))
-      x = self.pool(x)
+      # x = self.pool(x)
       x = self.flatten(x)
+      # print(x.shape)
       x = F.relu(self.fc1(x))
       return [x]
